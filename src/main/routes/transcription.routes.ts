ee9cc02954dce7e34c845f5a1360f2fba62da25f
José Lucas Gaspar <joselucas.gaspar@gmail.com>
2021-02-28 14:34:04 -0300

    feat: add DiskStorageProvider and make transcription route receive file

--- src/main/routes/transcription.routes.ts
+++ src/main/routes/transcription.routes.ts
@@ -1,10 +1,14 @@
 import { router } from '../protocols/IExpress';
+import { upload } from '../protocols/IMulter';
+import { uploadConfig } from '../config/upload';
 
-router.get('/transcrever', (req, res) => {
+const uploadFile = upload(uploadConfig.multer)
+
+router.get('/transcrever',  (req, res) => {
     res.json({ ok: true });
 });
 
-router.post('/transcrever', (req, res) => {
+router.post('/transcrever', uploadFile.single('file'), (req, res) => {
     res.json({ ok: false });
 });
 
--- src/presentation/controllers/TranscriptionController.ts
+++ src/presentation/controllers/TranscriptionController.ts
@@ -8,6 +8,8 @@ export class TranscriptionController implements IController {
         // Vai ter um UseCase pra mandar essa URI pra transcription da Amazon
         // Vai chamar o transcriptProvider que retorna um arquivo transcrito, da Amazon tbm
         // Vai ter um UseCase que pega esse arquivo e mostra pra Luiza
+
+        
         
         return new Promise(resolve => resolve({ statusCode: 100, body: 'oi' }));
     }
