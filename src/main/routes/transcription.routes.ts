832699637cefc083a9139cdc77f3185dcfd3ab60
José Lucas Gaspar <joselucas.gaspar@gmail.com>
2021-02-28 13:44:07 -0300

    refactor: deleted unnecessary comments

--- src/main/routes/transcription.routes.ts
+++ src/main/routes/transcription.routes.ts
@@ -1,12 +1,11 @@
-//import { Router } from 'express';
-import { router } from '../adapters/IExpress'
-
-//const router = Router()
+import { router } from '../adapters/IExpress';
 
 router.get('/transcrever', (req, res) => {
-    res.json({ ok: true })
+    res.json({ ok: true });
 });
 
-//router.post('/transcrever', );
+router.post('/transcrever', (req, res) => {
+    res.json({ ok: false });
+});
 
 export const transcriptionRoutes = router;
